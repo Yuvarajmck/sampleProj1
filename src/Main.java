7c0d9e5bcf2562622591eef711a71463b487ecdc
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 12:40:35 +0530

    String methods

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
           String msg = "hello";
         System.out.println(msg);
+          msg+=" world";
+        System.out.println(msg);
+        System.out.println(msg.endsWith("ld"));
     }
 }
