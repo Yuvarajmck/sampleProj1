632c708448889cf4c80f79a4370a267a13057a00
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 15:05:07 +0530

    MortgageCalculator

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Name : ");
-        String name = sc.nextLine();
-        System.out.println("you are "+name);
+        System.out.print("Principle : ");
+        int principle = sc.nextInt();
+        System.out.print("\nRate of interest : ");
+        float rateOfInterest = sc.nextFloat()/100/12;
+        System.out.print("\nno. of years : ");
+        int noOfYears = sc.nextInt()*12;
+        double mortgage = ( principle * rateOfInterest * Math.pow( (1+rateOfInterest), noOfYears))/(Math.pow((1+rateOfInterest), noOfYears) - 1);
+        System.out.println("Mortgage : "+mortgage);
     }
 }
