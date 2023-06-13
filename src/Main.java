09bed571444b7c987ead1dedb3159268d8d82be4
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 12:13:34 +0530

    ReferenceTypes

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        int age=30;
-        long viewCount = 1_234_355_869_798L;
-        float price = 12.06f;
-        double tax = 0.003;
-        char msg = 'h';
-        boolean isPeopleWatching = true;
-        System.out.println(age);
+        Date now = new Date();
+        System.out.println(now);
     }
 }
