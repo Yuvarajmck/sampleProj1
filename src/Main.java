e46cf912087604949f8726a3c50d0afd43689cf2
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 13:18:17 +0530

    arithmetic ops

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ public class Main {
           final float pi = 3.14F;
           float r = 2.17F;
         System.out.println(2*pi*r);
+        int a=5;
+        int b=10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        double d= (double)10/(double)8;
+        System.out.println(d);
     }
 }
