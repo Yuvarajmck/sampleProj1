242726ceb7b20140f1534780d83cc327136b9648
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 13:23:30 +0530

    typeCasting

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
-import java.lang.String;
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
           final float pi = 3.14F;
           float r = 2.17F;
-        System.out.println(2*pi*r);
-        int a=5;
-        int b=10;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        double d= (double)10/(double)8;
+          double d = r+2*pi;
         System.out.println(d);
     }
 }
