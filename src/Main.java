9f451b5b373ae29b92a8cdc85b59de61286055ac
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 14:46:34 +0530

    Scanner v1

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Age : ");
-        int age = sc.nextInt();
-        System.out.println("you are "+age);
+        System.out.print("Name : ");
+        String name = sc.nextLine();
+        System.out.println("you are "+name);
     }
 }
