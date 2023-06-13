3d9c76677008a406b01e22998079533a8d65c540
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 13:06:12 +0530

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
 import java.lang.String;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-          String msg = "hello";
-        System.out.println(msg);
-          msg+=" world";
-        System.out.println(msg);
-        System.out.println(msg.endsWith("ld"));
-        System.out.println(msg.toUpperCase());
-        System.out.println(msg.replace("world", "earth"));
-        System.out.println(msg.length());
-        String txt_msg = "he said \"hello maria\"";
-        String working_dir = "\"C:\\\\user\\\\dante\\\\coding-workspace\\\\java-space\\\\Main.java\"";
-        System.out.println(txt_msg);
-        System.out.println("Working dir : "+working_dir);
+          int[] nums = new int[5];
+          nums[0] = 3;
+          nums[1] = 76;
+        System.out.println(Arrays.toString(nums));
     }
 }
