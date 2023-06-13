e50ae52deb5e944dea52d6c888f943e95d5f03c5
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 11:54:10 +0530

    datatypes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int myAge=30;
-        int herAge=myAge;
-        System.out.println(herAge);
+        int age=30;
+        long viewCount = 1_234_355_869_798L;
+        float price = 12.06f;
+        double tax = 0.003;
+        char msg = 'h';
+        boolean isPeopleWatching = true;
+        System.out.println(age);
     }
 }
