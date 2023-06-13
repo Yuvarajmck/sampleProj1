f7d11c4d9d5641c889013aee59b0129189d92708
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 13:08:05 +0530

    arrays v1

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-          int[] nums = new int[5];
-          nums[0] = 3;
+          int[] nums = {1 ,23, 43, 0 ,0};
+          nums[4] = 37;
           nums[1] = 76;
         System.out.println(Arrays.toString(nums));
+        System.out.println(nums.length);
     }
 }
