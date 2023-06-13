1b63138f93c2a5e037779416c6008b0376b6f74a
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 15:14:50 +0530

    if else and operators

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Principle : ");
-        int principle = sc.nextInt();
-        System.out.print("\nRate of interest : ");
-        float rateOfInterest = sc.nextFloat()/100/12;
-        System.out.print("\nno. of years : ");
-        int noOfYears = sc.nextInt()*12;
-        double mortgage = ( principle * rateOfInterest * Math.pow( (1+rateOfInterest), noOfYears))/(Math.pow((1+rateOfInterest), noOfYears) - 1);
-        System.out.println("Mortgage : "+mortgage);
+        int a=(int)(Math.random()*100);
+        if(a>50){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+        boolean res = a>50;
+        String s = a>50 ? "It is more thn 50" : "it is less than 50";
+        System.out.println(s);
     }
 }
