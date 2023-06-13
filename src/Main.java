5fe8c4667415fad986bf0a739c332c685d762e15
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 14:45:27 +0530

    Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
 import java.lang.*;
-import java.text.NumberFormat;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-          final float pi = 3.14F;
-          float r = 2.17F;
-          double d = r+2*pi;
-          int x = (int)d+5;
-          String s = "10.0";
-        System.out.println(x);
-        System.out.println(Double.parseDouble(s));
-
-        NumberFormat perc = NumberFormat.getPercentInstance();
-        String res = perc.format(1234567.98765);
-        System.out.println(res);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Age : ");
+        int age = sc.nextInt();
+        System.out.println("you are "+age);
     }
 }
