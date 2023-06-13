10bfcd05abca86d21ac181d44d7ae4ed4741b656
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 13:11:41 +0530

    multi-dimensional arrays

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-          int[] nums = {1 ,23, 43, 0 ,0};
-          nums[4] = 37;
-          nums[1] = 76;
-        System.out.println(Arrays.toString(nums));
-        System.out.println(nums.length);
+          int[][] nums = new int[2][2];
+          nums[1][0] = 37;
+          nums[1][1] = 76;
+        System.out.println(Arrays.deepToString(nums));
+        System.out.println(nums[0].length);
     }
 }
