d7ac922ce13c9e4d1a01b50717e5fe1ff8f2fef7
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 13:13:59 +0530

    constant

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-          int[][] nums = new int[2][2];
-          nums[1][0] = 37;
-          nums[1][1] = 76;
-        System.out.println(Arrays.deepToString(nums));
-        System.out.println(nums[0].length);
+          final float pi = 3.14F;
+          float r = 2.17F;
+        System.out.println(2*pi*r);
     }
 }
