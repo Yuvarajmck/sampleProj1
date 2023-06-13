062f11702ccd340d5145dfd135af614987130b60
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 12:38:04 +0530

    String initialized

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
-import java.awt.*;
-
-
+import java.lang.String;
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(1, 2);
-        Point point2=point1;
-        System.out.println(point2);
-        point1.x=10;
-        System.out.println(point2);
+          String msg = "hello";
+        System.out.println(msg);
     }
 }
