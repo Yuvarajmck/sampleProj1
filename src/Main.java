f41a95768ddf521060ebb1c1bc880ad02fc6f55c
Yuvarajmck <yuvaraj.m@terzocloud.com>
2023-06-13 12:33:08 +0530

    References

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
-import java.util.Date;
+import java.awt.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println(now);
+        Point point1 = new Point(1, 2);
+        Point point2=point1;
+        System.out.println(point2);
+        point1.x=10;
+        System.out.println(point2);
     }
 }
